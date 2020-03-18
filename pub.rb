7b42f439a21957e631c87d164cc9cb82459d1e50
Tony Tang <teokigi@hotmail.com>
2020-03-18 12:20:21 +0000

    done pub, name and till test

--- pub.rb
+++ pub.rb
@@ -1,7 +1,14 @@
 class Pub
 
-    def initialize
+    attr_reader :name, :till
+
+    def initialize(name, drinks)
 #should have a name, a till and a collection of drinks
+        @name = name
+        @drinks = drinks
+        @till = 0
     end
+
+    
 
 end
--- specs/pub_specs.rb
+++ specs/pub_specs.rb
@@ -8,4 +8,19 @@ require_relative('../pub.rb')
 
 class PubTest < Minitest::Test
 #increase the money in the pubs till
+    def setup
+        @drink1 = Drink.new("Vodka",10)
+        @drink2 = Drink.new("Beer",20)
+        @drink3 = Drink.new("Gin",5)
+        @drinks = [@drink1,@drink2,@drink3]
+        @pub = Pub.new("The Kings Lost Arm", @drinks)
+    end
+
+    def test_001_get_name_of_pub
+        assert_equal("The Kings Lost Arm", @pub.name)
+    end
+
+    def test_002_get_till_amount
+        assert_equal(0, @pub.till)
+    end
 end
